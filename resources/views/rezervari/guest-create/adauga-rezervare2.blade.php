ea64cba8be194db61f3580dd2aa0d169bb5b21ef
‘Andrei <andrei.dima@usm.ro>
2019-07-10 00:48:53 +0300

    'diverse'

--- resources/views/rezervari/guest-create/adauga-rezervare2.blade.php
+++ resources/views/rezervari/guest-create/adauga-rezervare2.blade.php
@@ -26,131 +26,173 @@
             <div class="form-row">
                 <div class="col-md-2"></div>
                 <div class="col-md-8">
-                    <table class="">
+                    <table class="table m-0" style="border:5px solid #efe3b1; border-bottom:0px">
                         <tr style="text-align:center; font-weight:bold;">
-                            <td colspan="3" style="border-width:0px; padding:0rem;">
-                                <h3 style="background-color:#e7d790; color:black; margin:0px 0px 2px 0px; padding:2px 0px;">
-                                Informatii Calator
-                                </h3>
+                            <td colspan="" style="border-width:0px; padding:0rem;">
+                                <h4 style="background-color:#e7d790; color:black; margin:0px 0px 2px 0px; padding:2px 0px;">
+                                Informații Călător
+                                </h4>
+                            </td>
+                        </tr>
+                        <tr style="border-bottom:0px;">
+                            <td>
+                                Călător: <b>{{ $rezervare->nume }}</b>
+                            <br>
+                                Telefon: <b>{{ $rezervare->telefon }}</b>
+                            <br>
+                                E-mail: <b>{{ $rezervare->email }}</b>
+                            </td>
+                        </tr>                       
+                    </table>
+
+                    <table class="table m-0" style="border:5px solid #efe3b1; border-bottom:0px; border-top:0px;">    
+                        <tr style="text-align:center; font-weight:bold;">
+                            <td colspan="5" style="padding:0rem;">
+                                <h4 style="background-color:#e7d790; color:black; margin:0px 0px 2px 0px; padding:2px 0px">
+                                Informații Rezervare bilet
+                                </h4>
+                            </td>
+                        </tr>
+                        <tr valign="top">
+                            <td style="">
+                                Imbarcare: 
+                                <br>
+                                @if (!empty($rezervare->cursa->oras_plecare))
+                                    @if ($rezervare->cursa->oras_plecare->nume == "Otopeni")
+                                        <span style="font-size:1.2rem; font-weight:bold;">
+                                            Otopeni Aeroport
+                                        </span>
+                                    @else
+                                    <span style="font-size:1.2rem; font-weight:bold;">
+                                        {{ $rezervare->cursa->oras_plecare->nume }}
+                                    </span>
+                                    <br>
+                                    <span style="font-size:1rem;">
+                                        @if (!empty($rezervare->statie))
+                                            {{ $rezervare->statie->nume }}
+                                        @else
+                                            {{ $rezervare->statie_imbarcare }}
+                                        @endif
+                                    </span>
+                                    @endif
+                                @endif
+                            </td>
+                            <td style="">
+                                Plecare:
+                                <br>
+                                @if (!empty($rezervare->ora))
+                                    <span style="font-size:1.2rem; font-weight:bold;">
+                                        {{ \Carbon\Carbon::parse($rezervare->ora->ora)->format('H:i') }}
+                                    </span>
+                                @endif
+                                <br>
+                                {{-- @if (!empty(auth()->user())) --}}
+                                    <span style="font-size:1rem;">
+                                        {{ \Carbon\Carbon::parse($rezervare->data_cursa)->isoFormat('dddd') }}
+                                    </span>
+                                    <br>
+                                    {{ \Carbon\Carbon::parse($rezervare->data_cursa)->isoFormat('D MMM YYYY') }}
+                            </td>
+                            <td>
+                                <br>
+                                <img src="{{ asset('images/sageata.gif') }}" width="50px">
+                            </td>
+                            <td style="">
+                                Debarcare:
+                                <br>
+                                @if (!empty($rezervare->cursa->oras_sosire))
+                                    @if ($rezervare->cursa->oras_sosire->nume == "Otopeni")
+                                        <span style="font-size:1.2rem; font-weight:bold;">
+                                            Otopeni Aeroport
+                                        </span>
+                                    @else
+                                    <span style="font-size:1.2rem; font-weight:bold;">
+                                        {{ $rezervare->cursa->oras_sosire->nume }}
+                                    </span>
+                                    @endif
+                                @endif
+                            </td>
+                            <td style="">
+                                Sosire:
+                                <br>
+                                @if (!empty($rezervare->ora->ora && $rezervare->cursa->durata))
+                                    <span style="font-size:1.2rem; font-weight:bold;">
+                                        {{ \Carbon\Carbon::parse($rezervare->ora->ora)
+                                            ->addHours(\Carbon\Carbon::parse($rezervare->cursa->durata)->hour)
+                                            ->addMinutes(\Carbon\Carbon::parse($rezervare->cursa->durata)->minute)
+                                            ->format('H:i') }}   
+                                    </span>                          
+                                @endif
+                                <br>
+                                {{-- @if (!empty(auth()->user())) --}}
+                                    <span style="font-size:1rem;">
+                                        {{ \Carbon\Carbon::parse($rezervare->data_cursa)
+                                            ->addHours(\Carbon\Carbon::parse($rezervare->ora->ora)->hour)
+                                            ->addMinutes(\Carbon\Carbon::parse($rezervare->ora->ora)->minute) 
+                                            ->addHours(\Carbon\Carbon::parse($rezervare->cursa->durata)->hour)
+                                            ->addMinutes(\Carbon\Carbon::parse($rezervare->cursa->durata)->minute)                               
+                                            ->isoFormat('dddd') }}
+                                    </span>
+                                    <br>
+                                        {{ \Carbon\Carbon::parse($rezervare->data_cursa)
+                                            ->addHours(\Carbon\Carbon::parse($rezervare->ora->ora)->hour)
+                                            ->addMinutes(\Carbon\Carbon::parse($rezervare->ora->ora)->minute) 
+                                            ->addHours(\Carbon\Carbon::parse($rezervare->cursa->durata)->hour)
+                                            ->addMinutes(\Carbon\Carbon::parse($rezervare->cursa->durata)->minute)
+                                            ->isoFormat('D MMM YYYY') }}
+                                <br>
+                            </td>
+                        </tr>
+                    </table>
+                              
+                    <table class="table m-0 mb-2" style="border:5px solid #efe3b1;"> 
+                        <tr>
+                            <td>
+                                Nr. persoane: <b>{{ $rezervare->nr_adulti +  $rezervare->nr_copii }}</b>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                Total plata: <b>{{ $rezervare->pret_total }}</b>
+                                <br>
+                                <br>
+                                Stație îmbarcare:
+                                    @if (!empty($rezervare->statie))
+                                        <b>{{ $rezervare->statie->nume }}</b>
+                                    @endif
+                                <br>
+                                Detalii zbor:
+                                    <b>
+                                    {{ $rezervare->zbor_ora_decolare }}
+                                    -
+                                    {{ $rezervare->zbor_ora_aterizare }}
+                                    /
+                                    {{ $rezervare->zbor_oras_decolare}}
+                                    </b>
                             </td>
                         </tr>
                         <tr>
-                            <td width="35%" style="">
-                                Calator: <b>{{ $rezervari->nume }}</b>
-                            </td>
-                            <td width="25%" style="text-align:center;">
-                                Telefon: <b>{{ $rezervari->telefon }}</b>
-                            </td>
-                            <td width="40%" style="text-align:right;">
-                                E-mail: <b>{{ $rezervari->email }}</b>
+                            <td class="text-center">
+                                <small>
+                                    <p class="m-0">
+                                        Prin finalizarea rezervării sunteți de acord cu termenii și condițiile acestui site, precum și cu prelucrarea datelor cu caracter personal.
+                                    </p>                                
+                                    <span class="text-danger">
+                                        Neanunțarea telefonică cu minim 12 ore înainte de îmbarcare poate duce la pierderea biletelor rezervate!
+                                    </span>
+                                    <br>
+                                    <p class="m-0">
+                                        În funcție de numărul de pasageri circulă microbuz / autocar. Rezervarea este valabilă numai cu confirmarea agenției transportatoare.
+                                        Detalii la +40 786 574 788, +40 748 836 345, +40 766 862 890
+                                    </p>
+                                </small>
                             </td>
                         </tr>
-
-
-                        <tbody>
-                            <tr>
-                                <td class="text-right w-25 ">Client</td>
-                                <td class="w-75">{{ $rezervare->nume }}</td>
-                            </tr>
-                            <tr>
-                                <td class="text-right">Cursa</td>
-                                <td>
-                                    @if(!empty($rezervare->cursa)) 
-                                        {{ $rezervare->cursa->oras_plecare->nume }}
-                                        -                                         
-                                        {{ $rezervare->cursa->oras_sosire->nume }}
-                                    @endif
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="text-right w-25 ">Stație îmbarcare</td>
-                                <td class="w-75">
-                                    @if(!empty($rezervare->statie->nume)) 
-                                        {{ $rezervare->statie->nume }}
-                                    @endif
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="text-right">Dată cursă</td>
-                                <td>
-                                    {{-- @if(!empty($rezervare->data_cursa))  --}}
-                                    {{-- {{$rezervare->data_cursa}} --}}
-                                        {{-- {{ \Carbon\Carbon::parse($rezervare->data_cursa)->format('d.m.Y') }}, --}}
-                                        {{-- {{ \Carbon\Carbon::createFromFormat('Y.m.d H:i', $rezervare->data_cursa)->format('d.m.Y') }},
-                                    @endif
-                                    @if(!empty($rezervare->ora_id)) 
-                                        {{ \Carbon\Carbon::parse($rezervare->ora->ora)->format('H:i') }}
-                                    @endif --}}
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="text-right">De unde decolează avionul</td>
-                                <td>
-                                    {{ $rezervare->zbor_oras_decolare }}
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="text-right">Oră decolare</td>
-                                <td>
-                                    {{ $rezervare->zbor_ora_decolare }}
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="text-right">Ora aterizare</td>
-                                <td>
-                                    {{ $rezervare->zbor_ora_aterizare }}
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="text-right">Telefon</td>
-                                <td>
-                                    {{ $rezervare->telefon }}
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="text-right">Email</td>
-                                <td>
-                                    {{ $rezervare->email }}
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="text-right">Nr. adulți</td>
-                                <td>
-                                    {{ $rezervare->nr_adulti }}
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="text-right">Nr. copii</td>
-                                <td>
-                                    {{ $rezervare->nr_copii }}
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="text-right">Preț total</td>
-                                <td>
-                                    {{ $rezervare->pret_total }}
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="text-right">Comision agenție</td>
-                                <td>
-                                    {{ $rezervare->comision_agentie }}
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="text-right">Tip plată</td>
-                                <td>
-                                    @if(!empty($rezervare->tip_plata)) 
-                                        {{ $rezervare->tip_plata->nume }}
-                                    @endif
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="text-right">Observații</td>
-                                <td><pre style="white-space: pre-wrap;">{{ $rezervare->observatii }}</pre></td>
-                            </tr>
-                        </tbody>
                     </table>
+
+
+                    
+
+
+
                 </div>
             </div>
             <div class="form-row">
@@ -158,7 +200,7 @@
                     <form  class="needs-validation" novalidate method="POST" action="/adauga-rezervare-pasul-2">
                         @csrf                                                 
                             
-                        <button type="submit" class="btn btn-primary float-right" style="width: 100%; height:100%; white-space: normal;">Finalizează rezervarea</button> 
+                        <button type="submit" class="btn btn-primary" style="">Finalizează rezervarea</button> 
                     </form>
 
                 </div>
