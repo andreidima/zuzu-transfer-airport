13d3d08714c43653eab9cfd75bc1660c51ce134f
‘Andrei <andrei.dima@usm.ro>
2019-08-13 17:22:33 +0300

    ‘diverse’

--- resources/views/instructiuni_rezervari.blade.php
+++ resources/views/instructiuni_rezervari.blade.php
@@ -2,195 +2,142 @@
 
 @section('content') 
   
+<p><em>Pe această pagină gășiți instrucțiunile pt rezervări. Vor fi afișate și noutăți, schimbări de program sau modificări ale site-ului.</em></p>
 
-Pe această pagină gășiți instrucțiunile pt rezervări. Vor fi afișate și noutăți, schimbări de program sau modificări ale site-ului.
-
-PENTRU O MAI BUNĂ FUNCȚIONARE A SISTEMULUI VA RUGĂM SĂ FOLOSIȚI MOZILLA SAU CHROME!
-___________________________________
-Înainte de a face o rezervare asigurați-va că sunteți autentificat cu adresa de e-mail și parolă, la "Adaugă rezervare",
-	  alegeți orașul de plecare și orașul de sosire,
-	  selectați ora din grilă,
-	 alegeți dată doar din calendarul afișat,
-	  pentru plecările către Otopeni completați ora decolare avion,
-	  pentru plecările din Otopeni completați de unde este zborul și ora de aterizare (șoferul va sună pasagerul imediat după aterizare să-l îndrume către mașină, în parcarea de jos lângă bariere)
-	  nume și prenumele,
-	  telefon, dacă sunt mai multe numere se pune spațiu între ele,
-	  nr adulți și nr copii (între 2 și 7 ani) dacă este cazul,
-	  la Stație de îmbarcare completați de unde se face îmbarcarea, dacă este cazul se adaugă și "debarcare/....." dacă este diferită,
- 
-
-•	BRĂILA: Plantelor, Penny pe Bz-ului, Catedrala la covrigaria Panda, 
-•	IANCA: la Avion.
-•	GALAȚI:  la McDonald`s.
-•	BUZĂU: McDonald`s, peco Alascom sau la Spătaru unde se face și un popas.
-•	FOCȘANI:  la Lidl sau Lukoil în centru,
-•	RÂMNICU SĂRAT:  la Turist-Profi,
-•	TECUCI:  în parcare la Kaufland,
-•	PANCIU: Notar,
-•	ADJUD: Atlantic sau Lukoil,
-•	MĂRĂȘEȘTI: Gara.
- 
-	scrieți sau selectați cu săgeata nr de persoane (adulți sau copii),
-	dacă încasați comisionul în agenție completați suma în căsuța de comision și apoi selectați “Adaugă rezervarea”
-	dacă plata se va face la șofer selectați “Adaugă rezervarea” fără să mai bifați/ completați altceva,
-	dacă încasați integral la agenție bifați “Preț total:   La agenție”
-	dacă se solicită și retur bifați “Retur”, completați datele de întoarcere și apoi selectați “Adaugă rezervarea”
-	verificați datele introduse apoi selectați “Adaugă rezervarea”, rezervarea s-a înregistrat, puteți descarcă biletul.
-	Pentru a reveni la pagină principala selectați “Rezervări” din meniul de sus.
-
-Aveți posibilitatea de a modifică nr. de telefon și stația de îmbarcare după ce ați făcut rezervarea, de la iconiță de editare din dreptul rezervarii.
-Pentru alte modificări, rezervări greșite sau anulări ne puteți contacta telefonic sau pe mail la adresa carabus25@yahoo.com .
-În meniul de sus la “Raport” se pot lista rezervările pe un interval de timp pe care îl selectați din calendar.
-•	Orice altă observație (îmbarcare/debarcare, altă ora dacă e cursa specială, etc )  care trebuie adăugată la rezervare și pt care nu există o căsuța de completare să o scrieți la “Stație de îmbarcare. Este la fel de important să scrieți stația de îmbarcare la plecările către Otopeni, sunt cazuri, mai ales în Brăila, când persoanele nu răspund la telefon și șoferul nu știe de unde să le ia.
-•	Rezervările care va apar evidențiate cu albastru, sunt anulate de către noi pe motiv că nu s-au prezentat sau au fost anulate la cerere de către clienți telefonic.
-      
-Noi sunăm persoanele înainte cu o zi pentru a confirmă rezervările pentru plecările spre Otopeni, pentru retur se trimit sms-uri, nu vor fi sunați cei care fac rezervarea cu o zi înainte sau în aceeași zi cu plecarea, scopul este de a verifica datele înregistrate pe rezervare: ora, dată, stație îmbarcare unde de multe ori nu este scrisă.
-      Dacă datele rezervarii nu sunt corecte există riscul că acea rezervare să fie nulă. Rezervările sunt și rămân valabile și fără o confirmare dacă acestea sunt introduse corect. 
- 
-      REZERVĂRILE PT A DOUA ZI (aceasta începând cu ora 00:00) SE FAC PÂNĂ LA ORA 20:50, IAR PT ZIUA ÎN CURS SE VOR ANUNȚA TELEFONIC LA DISPECERAT   0767 335 558 / 0748 836 345 PT A VERIFICA DISPONIBILITATEA LOCURILOR!   
-
-
-TRASEU	PRET 	COMISION AGENTIE 
-TECUCI,  PANCIU,  ADJUD, ↔OTOPENI	80 lei/adult (40 lei/copil*)	20lei   (5 lei)
-MARASESTI↔OTOPENI
-	70 lei/adult (35 lei/copil*)	15 lei   (5 lei)
-FOCSANI↔OTOPENI	60 lei/adult  (30 lei/copil)
-	15 lei   (5 lei)
-RM. SARAT↔OTOPENI	50 lei/adult  (25 lei/copil)	10 lei   (5 lei)
-
-BUZAU↔OTOPENI	40 lei/adult  (20 lei/copil)
-	10 lei   (5 lei)
-GALATI/BRAILA↔OTOPENI
-	70 lei/adult  (40 lei/copil)	20 lei (10 lei)
-*copil cu vârstă cuprinsă între 2 și 7 ani 
-
-<p><em>Pe această&nbsp;pagină&nbsp;gășiți&nbsp;instrucțiunile&nbsp;pt&nbsp;rezervări. Vor&nbsp;fi&nbsp;afișate&nbsp;și&nbsp;noutăți,&nbsp;schimbări&nbsp;de program&nbsp;sau&nbsp;modificări&nbsp;ale&nbsp;site-ului.</em></p>
-<p><strong>PENTRU O&nbsp;MAI&nbsp;BUNĂ&nbsp;FUNCȚIONARE&nbsp;A&nbsp;SISTEMULUI&nbsp;VA&nbsp;RUGĂM&nbsp;SĂ&nbsp;FOLOSIȚI&nbsp;MOZILLA&nbsp;SAU&nbsp;CHROME!</strong></p>
+<p><strong>PENTRU O MAI BUNĂ FUNCȚIONARE A SISTEMULUI VA RUGĂM SĂ FOLOSIȚI MOZILLA SAU CHROME!</strong></p>
 <p>___________________________________</p>
-<p>&Icirc;nainte&nbsp;de a face o rezervare&nbsp;asigurați-va&nbsp;că&nbsp;sunteți&nbsp;autentificat cu&nbsp;adresa&nbsp;de e-mail&nbsp;și&nbsp;parolă,&nbsp;la&nbsp;"<strong>Adaugă&nbsp;rezervare</strong>",</p>
-<ul>
-<li>alegeți&nbsp;orașul&nbsp;de plecare&nbsp;și&nbsp;orașul&nbsp;de sosire,</li>
-<li>selectați&nbsp;ora&nbsp;din&nbsp;grilă,</li>
-<li>alegeți&nbsp;dată&nbsp;doar&nbsp;din&nbsp;calendarul&nbsp;afișat,</li>
-<li>pentru&nbsp;plecările&nbsp;către&nbsp;Otopeni&nbsp;completați&nbsp;<strong>ora&nbsp;decolare</strong>&nbsp;avion,</li>
-<li>pentru plecările din Otopeni completați de unde este zborul și ora de aterizare (șoferul va sună pasagerul imediat după aterizare să-l îndrume către mașină, în parcarea de jos lângă bariere)</li>
-<li>nume&nbsp;și&nbsp;prenumele,</li>
-<li>telefon,&nbsp;dacă&nbsp;sunt&nbsp;mai&nbsp;multe&nbsp;numere se pune&nbsp;spațiu&nbsp;&icirc;ntre&nbsp;ele,</li>
-<li>nr&nbsp;adulți&nbsp;și&nbsp;nr copii (&icirc;ntre&nbsp;2&nbsp;și&nbsp;7 ani)&nbsp;dacă&nbsp;este cazul,</li>
-<li>la&nbsp;<strong>Stație&nbsp;de&nbsp;&icirc;mbarcare</strong>&nbsp;completați&nbsp;de unde&nbsp;se&nbsp;face&nbsp;&icirc;mbarcarea,&nbsp;dacă&nbsp;este cazul se&nbsp;adaugă&nbsp;și&nbsp;"debarcare/....."&nbsp;dacă&nbsp;este&nbsp;diferită,</li>
+
+<p>Înainte de a face o rezervare asigurați-va că sunteți autentificat cu adresa de e-mail și parolă, la "<b>Adaugă rezervare</b>",</p>
+<ul style="list-style-type: circle">
+    <li>alegeți orașul de plecare și orașul de sosire,</li>
+    <li>selectați ora din grilă,</li>
+    <li>alegeți dată doar din calendarul afișat,</li>
+    <li>pentru plecările către Otopeni completați ora decolare avion,</li>
+    <li>pentru plecările din Otopeni completați de unde este zborul și ora de aterizare (șoferul va sună pasagerul imediat după aterizare să-l îndrume către mașină, în parcarea de jos lângă bariere)</li>
+    <li>nume și prenumele,</li>
+    <li>telefon, dacă sunt mai multe numere se pune spațiu între ele,</li>
+    <li>nr adulți și nr copii (între 2 și 7 ani) dacă este cazul,</li>
+    <li>la Stație de îmbarcare completați de unde se face îmbarcarea, dacă este cazul se adaugă și "debarcare/....." dacă este diferită,</li>
 </ul>
-<ol>
-<li><strong>BRĂILA</strong>: Plantelor,&nbsp;Penny&nbsp;pe Bz-ului,&nbsp;Catedrala&nbsp;la&nbsp;covrigaria&nbsp;Panda,</li>
-<li><strong>IANCA:</strong> la Avion.</li>
-<li><strong>GALAȚI:&nbsp;</strong>&nbsp;la&nbsp;McDonald`s.</li>
-<li><strong>BUZĂU</strong>: McDonald`s, peco Alascom&nbsp;sau&nbsp;la&nbsp;Spătaru&nbsp;unde&nbsp;se&nbsp;face&nbsp;și&nbsp;un popas.</li>
-<li><strong>FOCȘANI:&nbsp;</strong>&nbsp;la&nbsp;Lidl&nbsp;sau&nbsp;Lukoil&nbsp;&icirc;n&nbsp;centru,</li>
-<li><strong>R&Acirc;MNICU&nbsp;SĂRAT:&nbsp;</strong>&nbsp;la&nbsp;Turist-Profi,</li>
-<li><strong>TECUCI:&nbsp;</strong>&nbsp;&icirc;n&nbsp;parcare&nbsp;la&nbsp;Kaufland,</li>
-<li><strong>PANCIU:&nbsp;</strong>Notar,</li>
-<li><strong>ADJUD</strong>: Atlantic&nbsp;sau&nbsp;Lukoil,</li>
-<li><strong>MĂRĂȘEȘTI</strong>:&nbsp;Gara.</li>
-</ol>
-<ul>
-<li>scrieți&nbsp;sau&nbsp;selectați&nbsp;cu&nbsp;săgeata&nbsp;<strong>nr de persoane</strong>(adulți&nbsp;sau copii),</li>
-<li>dacă&nbsp;&icirc;ncasați&nbsp;comisionul&nbsp;&icirc;n&nbsp;agenție&nbsp;completați&nbsp;suma&nbsp;&icirc;n&nbsp;căsuța&nbsp;de comision&nbsp;și&nbsp;apoi&nbsp;selectați&nbsp;&ldquo;<strong>Adaugă&nbsp;rezervarea</strong>&rdquo;</li>
-<li>dacă&nbsp;plata&nbsp;se&nbsp;va&nbsp;face&nbsp;la&nbsp;șofer&nbsp;selectați&nbsp;&ldquo;<strong>Adaugă&nbsp;rezervarea</strong>&rdquo;&nbsp;fără&nbsp;să&nbsp;mai&nbsp;bifați/&nbsp;completați&nbsp;altceva,</li>
-<li>dacă&nbsp;&icirc;ncasați&nbsp;integral&nbsp;la&nbsp;agenție&nbsp;bifați&nbsp;&ldquo;<strong>Preț&nbsp;total: La&nbsp;agenție</strong>&rdquo;</li>
-<li>dacă&nbsp;se&nbsp;solicită&nbsp;și&nbsp;retur&nbsp;bifați&nbsp;&ldquo;<strong>Retur</strong>&rdquo;,&nbsp;completați&nbsp;datele de&nbsp;&icirc;ntoarcere&nbsp;și&nbsp;apoi&nbsp;selectați&nbsp;&ldquo;<strong>Adaugă&nbsp;rezervarea</strong>&rdquo;</li>
-<li>verificați&nbsp;datele introduse apoi&nbsp;selectați&nbsp;&ldquo;<strong>Adaugă&nbsp;rezervarea</strong>&rdquo;, rezervarea s-a&nbsp;&icirc;nregistrat,&nbsp;puteți&nbsp;descarcă&nbsp;biletul.</li>
-<li>Pentru a reveni&nbsp;la&nbsp;pagină&nbsp;principala&nbsp;selectați&nbsp;&ldquo;<strong>Rezervări</strong>&rdquo;&nbsp;din&nbsp;meniul de&nbsp;sus.</li>
+
+<ul style="list-style-type: square">
+    <li>BRĂILA: Plantelor, Penny pe Bz-ului, Catedrala la covrigaria Panda,</li>
+    <li>IANCA: la Avion.</li>
+    <li>GALAȚI:  la McDonald`s.</li>
+    <li>BUZĂU: McDonald`s, peco Alascom sau la Spătaru unde se face și un popas.</li>
+    <li>FOCȘANI:  la Lidl sau Lukoil în centru,</li>
+    <li>RÂMNICU SĂRAT:  la Turist-Profi,</li>
+    <li>TECUCI:  în parcare la Kaufland,</li>
+    <li>PANCIU: Notar,</li>
+    <li>ADJUD: Atlantic sau Lukoil,</li>
+    <li>MĂRĂȘEȘTI: Gara.</li>
 </ul>
-<p><strong>Aveți&nbsp;posibilitatea&nbsp;de a&nbsp;modifică&nbsp;nr. de telefon&nbsp;și&nbsp;stația&nbsp;de&nbsp;&icirc;mbarcare&nbsp;după&nbsp;ce&nbsp;ați&nbsp;făcut&nbsp;rezervarea, de&nbsp;la&nbsp;iconiță&nbsp;de editare&nbsp;din&nbsp;dreptul&nbsp;rezervarii.</strong></p>
-<p><strong>Pentru alte&nbsp;modificări,&nbsp;rezervări&nbsp;greșite&nbsp;sau&nbsp;anulări&nbsp;ne&nbsp;puteți&nbsp;contacta telefonic sau pe mail&nbsp;la&nbsp;adresa&nbsp;</strong><strong><a href="mailto:carabus25@yahoo.com">carabus25@yahoo.com</a></strong><strong>&nbsp;.</strong></p>
-<p>&Icirc;n&nbsp;meniul de&nbsp;sus&nbsp;la&nbsp;&ldquo;<strong>Raport</strong>&rdquo;&nbsp;se&nbsp;pot&nbsp;lista&nbsp;rezervările&nbsp;pe un interval de&nbsp;timp&nbsp;pe&nbsp;care&nbsp;&icirc;l&nbsp;selectați&nbsp;din&nbsp;calendar.</p>
-<ul>
-<li>Orice&nbsp;altă&nbsp;observație&nbsp;(&icirc;mbarcare/debarcare,&nbsp;altă&nbsp;ora&nbsp;dacă&nbsp;e&nbsp;cursa&nbsp;specială, etc ) care&nbsp;trebuie&nbsp;adăugată&nbsp;la&nbsp;rezervare&nbsp;și&nbsp;pt care nu&nbsp;există&nbsp;o&nbsp;căsuța&nbsp;de completare&nbsp;să&nbsp;o&nbsp;scrieți&nbsp;la &ldquo;<strong>Stație&nbsp;de&nbsp;&icirc;mbarcare</strong>. Este la fel de&nbsp;important&nbsp;să&nbsp;scrieți&nbsp;stația&nbsp;de&nbsp;&icirc;mbarcare&nbsp;la&nbsp;plecările&nbsp;către&nbsp;Otopeni,&nbsp;sunt&nbsp;cazuri,&nbsp;mai&nbsp;ales&nbsp;&icirc;n&nbsp;Brăila,&nbsp;c&acirc;nd&nbsp;persoanele nu&nbsp;răspund&nbsp;la telefon&nbsp;și&nbsp;șoferul&nbsp;nu&nbsp;știe&nbsp;de unde&nbsp;să&nbsp;le ia.</li>
-<li>Rezervările&nbsp;care&nbsp;va&nbsp;apar&nbsp;evidențiate&nbsp;cu albastru,&nbsp;sunt&nbsp;anulate de&nbsp;către&nbsp;noi pe motiv&nbsp;că&nbsp;nu s-au prezentat&nbsp;sau&nbsp;au fost anulate&nbsp;la&nbsp;cerere de&nbsp;către&nbsp;clienți&nbsp;telefonic.</li>
+
+<ul style="list-style-type: circle">
+    <li>scrieți sau selectați cu săgeata nr de persoane (adulți sau copii),</li>
+    <li>dacă încasați comisionul în agenție completați suma în căsuța de comision și apoi selectați “Adaugă rezervarea”</li>
+    <li>dacă plata se va face la șofer selectați “Adaugă rezervarea” fără să mai bifați/ completați altceva,</li>
+    <li>dacă încasați integral la agenție bifați “Preț total: La agenție”</li>
+    <li>dacă se solicită și retur bifați “Retur”, completați datele de întoarcere și apoi selectați “Adaugă rezervarea”</li>
+    <li>verificați datele introduse apoi selectați “Adaugă rezervarea”, rezervarea s-a înregistrat, puteți descarcă biletul.</li>
+    <li>Pentru a reveni la pagină principala selectați “Rezervări” din meniul de sus.</li>
 </ul>
-<p><br /><strong>Noi&nbsp;sunăm&nbsp;persoanele&nbsp;&icirc;nainte&nbsp;cu o zi pentru a&nbsp;confirmă&nbsp;rezervările&nbsp;pentru&nbsp;plecările&nbsp;spre Otopeni, pentru retur&nbsp;se&nbsp;trimit&nbsp;sms-uri, nu vor&nbsp;fi&nbsp;sunați&nbsp;cei&nbsp;care&nbsp;fac rezervarea cu o zi&nbsp;&icirc;nainte&nbsp;sau&nbsp;&icirc;n&nbsp;aceeași&nbsp;zi cu plecarea, scopul este de a&nbsp;verifica&nbsp;datele&nbsp;&icirc;nregistrate&nbsp;pe rezervare:&nbsp;ora,&nbsp;dată,&nbsp;stație&nbsp;&icirc;mbarcare&nbsp;unde de&nbsp;multe&nbsp;ori nu este&nbsp;scrisă.</strong></p>
-<p><strong>Dacă&nbsp;datele&nbsp;rezervarii&nbsp;nu&nbsp;sunt&nbsp;corecte&nbsp;există&nbsp;riscul&nbsp;că&nbsp;acea rezervare&nbsp;să&nbsp;fie&nbsp;nulă.&nbsp;Rezervările&nbsp;sunt&nbsp;și&nbsp;răm&acirc;n&nbsp;valabile&nbsp;și&nbsp;fără&nbsp;o confirmare&nbsp;dacă&nbsp;acestea&nbsp;sunt introduse corect.&nbsp;</strong></p>
-<p><strong>REZERVĂRILE&nbsp;PT A&nbsp;DOUA&nbsp;ZI (aceasta&nbsp;&icirc;ncep&acirc;nd&nbsp;cu&nbsp;ora&nbsp;00:00)&nbsp;SE&nbsp;FAC&nbsp;P&Acirc;NĂ&nbsp;LA&nbsp;ORA&nbsp;20:50, IAR PT&nbsp;ZIUA&nbsp;&Icirc;N&nbsp;CURS SE VOR&nbsp;ANUNȚA&nbsp;TELEFONIC LA DISPECERAT 0767 335 558 / 0748 836 345 PT A&nbsp;VERIFICA&nbsp;DISPONIBILITATEA LOCURILOR!</strong></p>
-<table width="765" style="border:1px solid black">
-<tbody>
-<tr>
-<td width="304">
-<p><strong>TRASEU</strong></p>
-</td>
-<td width="236">
-<p><strong>PRET</strong></p>
-</td>
-<td width="224">
-<p><strong>COMISION AGENTIE</strong></p>
-</td>
-</tr>
-<tr>
-<td width="304">
-<p>TECUCI, PANCIU, ADJUD, &harr;OTOPENI</p>
-</td>
-<td width="236">
-<p>80 lei/adult&nbsp;(40 lei/copil*)</p>
-</td>
-<td width="224">
-<p>20lei (5 lei)</p>
-</td>
-</tr>
-<tr>
-<td width="304">
-<p>MARASESTI&harr;OTOPENI</p>
-</td>
-<td width="236">
-<p>70 lei/adult (35 lei/copil*)</p>
-</td>
-<td width="224">
-<p>15 lei (5 lei)</p>
-</td>
-</tr>
-<tr>
-<td width="304">
-<p>FOCSANI&harr;OTOPENI</p>
-</td>
-<td width="236">
-<p>60 lei/adult (30 lei/copil)</p>
-</td>
-<td width="224">
-<p>15 lei (5 lei)</p>
-</td>
-</tr>
-<tr>
-<td width="304">
-<p>RM. SARAT&harr;OTOPENI</p>
-</td>
-<td width="236">
-<p>50 lei/adult (25 lei/copil)</p>
-</td>
-<td width="224">
-<p>10 lei (5 lei)</p>
-</td>
-</tr>
-<tr>
-<td width="304">
-<p>BUZAU&harr;OTOPENI</p>
-</td>
-<td width="236">
-<p>40 lei/adult (20 lei/copil)</p>
-</td>
-<td width="224">
-<p>10 lei (5 lei)</p>
-</td>
-</tr>
-<tr>
-<td width="304">
-<p>GALATI/BRAILA&harr;OTOPENI</p>
-</td>
-<td width="236">
-<p>70 lei/adult (40 lei/copil)</p>
-</td>
-<td width="224">
-<p>20 lei (10 lei)</p>
-</td>
-</tr>
-</tbody>
+
+<p><b>Aveți posibilitatea de a modifică nr. de telefon și stația de îmbarcare după ce ați făcut rezervarea, de la iconiță de editare din dreptul rezervarii.</b></p>
+<p><b>Pentru alte modificări, rezervări greșite sau anulări ne puteți contacta telefonic sau pe mail la adresa <a href="mailto:carabus25@yahoo.com">carabus25@yahoo.com.</a></b></p>
+<p>În meniul de sus la „<b>Raport</b>” se pot lista rezervările pe un interval de timp pe care îl selectați din calendar.</p>
+<ul>
+    <li>Orice altă observație (îmbarcare/debarcare, altă ora dacă e cursa specială, etc )  care trebuie adăugată la rezervare și pt care nu există o căsuța de completare să o scrieți la “Stație de îmbarcare. Este la fel de important să scrieți stația de îmbarcare la plecările către Otopeni, sunt cazuri, mai ales în Brăila, când persoanele nu răspund la telefon și șoferul nu știe de unde să le ia.</li>
+    <li>Rezervările care va apar evidențiate cu albastru, sunt anulate de către noi pe motiv că nu s-au prezentat sau au fost anulate la cerere de către clienți telefonic.</li>
+</ul>
+      
+<p><b>Noi sunăm persoanele înainte cu o zi pentru a confirmă rezervările pentru plecările spre Otopeni, pentru retur se trimit sms-uri, nu vor fi sunați cei care fac rezervarea cu o zi înainte sau în aceeași zi cu plecarea, scopul este de a verifica datele înregistrate pe rezervare: ora, dată, stație îmbarcare unde de multe ori nu este scrisă.</b></p>
+<p><b>Dacă datele rezervarii nu sunt corecte există riscul că acea rezervare să fie nulă. Rezervările sunt și rămân valabile și fără o confirmare dacă acestea sunt introduse corect. </b></p>
+ 
+<p><b>REZERVĂRILE PT A DOUA ZI (aceasta începând cu ora 00:00) SE FAC PÂNĂ LA ORA 20:50, IAR PT ZIUA ÎN CURS SE VOR ANUNȚA TELEFONIC LA DISPECERAT   0767 335 558 / 0748 836 345 PT A VERIFICA DISPONIBILITATEA LOCURILOR!</b></p>
+
+<p>&nbsp;</p>
+<table style="border:1px solid black; border-collapse: collapse; text-align:center;" align="center">
+    <tr>
+        <td class="p-2" style="border:1px solid black;">
+            TRASEU
+        </td>
+        <td class="p-2" style="border:1px solid black;">
+            PRET 
+        </td>
+        <td class="p-2" style="border:1px solid black;">
+            COMISION AGENTIE
+        </td>
+    </tr>
+    <tr>
+        <td class="p-2" style="border:1px solid black;">
+            TECUCI,  PANCIU,  ADJUD 
+            <br>
+            ↔ OTOPENI
+        </td>
+        <td class="p-2" style="border:1px solid black;">
+            80 lei/adult (40 lei/copil*)
+        </td>
+        <td class="p-2" style="border:1px solid black;">
+            20lei (5 lei)
+        </td>
+    </tr>
+    <tr>
+        <td class="p-2" style="border:1px solid black;">
+            MARASESTI ↔ OTOPENI
+        </td>
+        <td class="p-2" style="border:1px solid black;">
+            70 lei/adult (35 lei/copil*)
+        </td>
+        <td class="p-2" style="border:1px solid black;">
+            15 lei (5 lei)
+        </td>
+    </tr>
+    <tr>
+        <td class="p-2" style="border:1px solid black;">
+            FOCSANI ↔ OTOPENI
+        </td>
+        <td class="p-2" style="border:1px solid black;">
+            60 lei/adult  (30 lei/copil)
+        </td>
+        <td class="p-2" style="border:1px solid black;">
+            15 lei (5 lei)
+        </td>
+    </tr>
+    <tr>
+        <td class="p-2" style="border:1px solid black;">
+            RM. SARAT ↔ OTOPENI
+        </td>
+        <td class="p-2" style="border:1px solid black;">
+            50 lei/adult  (25 lei/copil)
+        </td>
+        <td class="p-2" style="border:1px solid black;">
+            10 lei (5 lei)
+        </td>
+    </tr>
+    <tr>
+        <td class="p-2" style="border:1px solid black;">
+            BUZAU ↔ OTOPENI
+        </td>
+        <td class="p-2" style="border:1px solid black;">
+             40 lei/adult  (20 lei/copil)
+        </td>
+        <td class="p-2" style="border:1px solid black;">
+            10 lei   (5 lei)
+        </td>
+    </tr>
+    <tr>
+        <td class="p-2" style="border:1px solid black;">
+            GALATI/BRAILA ↔ OTOPENI
+        </td>
+        <td class="p-2" style="border:1px solid black;">
+             70 lei/adult  (40 lei/copil)
+        </td>
+        <td class="p-2" style="border:1px solid black;">
+            20 lei (10 lei)
+        </td>
+    </tr>
 </table>
-<p>*copil cu&nbsp;v&acirc;rstă&nbsp;cuprinsă&nbsp;&icirc;ntre&nbsp;2&nbsp;și&nbsp;7 ani&nbsp;</p>
+<p style="text-align:center;">*copil cu vârstă cuprinsă între 2 și 7 ani</p>
 
 @endsection
