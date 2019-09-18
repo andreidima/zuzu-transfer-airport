2b9b205f2ba1e03e7e0c13fe3793c8cf3c9d9e2a
‘Andrei <andrei.dima@usm.ro>
2019-09-18 15:22:35 +0300

    ‘diverse’

--- app/Http/Controllers/RezervareController.php
+++ app/Http/Controllers/RezervareController.php
@@ -785,7 +785,7 @@ class RezervareController extends Controller
 
     public function testarePlataCard1(Request $request)
     {
-        $comanda = Mobilpay::setOrderId(1)
+        $comanda = Mobilpay::setOrderId(2)
         ->setAmount('10.00')
         ->setDetails('Some details')
         // ->setReturnUrl('https://www.youtube.com');
