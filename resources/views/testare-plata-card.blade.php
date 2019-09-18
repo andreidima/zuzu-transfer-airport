e8685841c9945db6c410e38005649044cbc1e185
‘Andrei <andrei.dima@usm.ro>
2019-09-18 16:29:13 +0300

    ‘diverse’

--- resources/views/testare-plata-card.blade.php
+++ resources/views/testare-plata-card.blade.php
@@ -4,6 +4,6 @@
 
 Esti in pagina de testare plata card. Modificata.
 
-<a class="btn btn-primary" href="/testare-plata-card1" role="button">Plateste</a>
+<a class="btn btn-primary" href="/trimitere-catre-plata" role="button">Plateste</a>
 @endsection
 
