7d0f610bc97bed6b6c481ce7731bca0f5fbed567
‘Andrei <andrei.dima@usm.ro>
2019-09-18 13:38:19 +0300

    ‘diverse’

--- resources/views/testare-plata-card.blade.php
+++ resources/views/testare-plata-card.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-Esti in pagina de testare plata card
+Esti in pagina de testare plata card. Modificata.
 
 <a class="btn btn-primary" href="/testare-plata-card1" role="button">Plateste</a>
 @endsection
