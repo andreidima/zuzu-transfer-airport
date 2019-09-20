a7c7b573cba064fbb249706b24ea805ba1567b0c
‘Andrei <andrei.dima@usm.ro>
2019-09-20 18:43:58 +0300

    'diverse'

--- app/Http/Controllers/PlataOnlineController.php
+++ app/Http/Controllers/PlataOnlineController.php
@@ -72,7 +72,7 @@ class PlataOnlineController extends Controller
             'original_amount' => $data['objPmNotify']['originalAmount'],
             'processed_amount' => $data['objPmNotify']['processedAmount'],
             'rezervare_id' => 'a',
-            'nume' => ['customer']['firstName'],
+            'nume' => ['objPmNotify']['firstName'],
             // 'nume' => ['objPmNotify']['customer']['firstName'],
             // 'telefon' => ['objPmNotify']['customer']['mobilePhone'],
             // 'email' => ['objPmNotify']['customer']['email'],
