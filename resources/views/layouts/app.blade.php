49faf0f005337805190270f712c31e725aa3d266
‘Andrei <andrei.dima@usm.ro>
2019-09-21 17:19:17 +0300

    'diverse'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -141,13 +141,13 @@
                                         </form>                                         
                                     @endif
 
-                                    <div class="dropdown-divider"></div> 
+                                    <div class="dropdown-divider"></div>  --}}
 
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
-                                    </a> --}}
+                                    </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
