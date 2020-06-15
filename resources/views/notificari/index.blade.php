d2524a770ae2607269d465c2d2be9013ff154a97
‘Andrei <andrei.dima@usm.ro>
2020-06-15 16:07:56 +0300

    'oferte-si-notificari'

--- resources/views/notificari/index.blade.php
+++ resources/views/notificari/index.blade.php
@@ -87,7 +87,7 @@
                                             <div class="modal-dialog modal-dialog-centered" role="document">
                                                 <div class="modal-content">
                                                 <div class="modal-header bg-warning">
-                                                    <h5 class="modal-title" id="exampleModalLabel">Notificare: <b>{{ $notificare->nume }}</b></h5>
+                                                    <h5 class="modal-title" id="exampleModalLabel">Notificare: <b>{{ $notificare->text }}</b></h5>
                                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                     <span aria-hidden="true">&times;</span>
                                                     </button>
