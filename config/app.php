561e3a9023a0a7ce05c76dbfe7f29e80c07f8df0
Andrei Dima <andrei.dima@usm.ro>
2023-03-26 14:27:03 +0300

    a

--- config/app.php
+++ config/app.php
@@ -67,7 +67,8 @@ return [
     |
     */
 
-    'timezone' => 'Europe/Bucharest',
+    // 'timezone' => 'Europe/Bucharest',
+    'timezone' => '',
 
     /*
     |--------------------------------------------------------------------------
